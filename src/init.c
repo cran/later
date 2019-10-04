1d0be6b6ae6f2da6662287d978533bcd164f6cd7
Joe Cheng <joe@rstudio.com>
2019-10-04 04:00:02 +0000

    version 1.0.0

--- src/init.c
+++ src/init.c
@@ -2,34 +2,69 @@
 #include <Rinternals.h>
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
+#include <stdint.h> // for uint64_t
 
-/* FIXME: 
+/* FIXME:
 Check these declarations against the C/Fortran source code.
 */
 
 /* .Call calls */
 extern SEXP _later_ensureInitialized();
-extern SEXP _later_execCallbacks(SEXP, SEXP);
-extern SEXP _later_idle();
-extern SEXP _later_execLater(SEXP, SEXP);
-extern SEXP _later_next_op_secs();
+extern SEXP _later_execCallbacks(SEXP, SEXP, SEXP);
+extern SEXP _later_idle(SEXP);
+extern SEXP _later_execLater(SEXP, SEXP, SEXP);
+extern SEXP _later_cancel(SEXP, SEXP);
+extern SEXP _later_nextOpSecs(SEXP);
 extern SEXP _later_testCallbackOrdering();
+extern SEXP _later_createCallbackRegistry(SEXP);
+extern SEXP _later_deleteCallbackRegistry(SEXP);
+extern SEXP _later_existsCallbackRegistry(SEXP);
+extern SEXP _later_list_queue_(SEXP);
+extern SEXP _later_log_level(SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-  {"_later_ensureInitialized",    (DL_FUNC) &_later_ensureInitialized,    0},
-  {"_later_execCallbacks",        (DL_FUNC) &_later_execCallbacks,        2},
-  {"_later_idle",                 (DL_FUNC) &_later_idle,                 0},
-  {"_later_execLater",            (DL_FUNC) &_later_execLater,            2},
-  {"_later_next_op_secs",         (DL_FUNC) &_later_next_op_secs,         0},
+  {"_later_ensureInitialized",   (DL_FUNC) &_later_ensureInitialized,   0},
+  {"_later_execCallbacks",       (DL_FUNC) &_later_execCallbacks,       3},
+  {"_later_idle",                (DL_FUNC) &_later_idle,                1},
+  {"_later_execLater",           (DL_FUNC) &_later_execLater,           3},
+  {"_later_cancel",              (DL_FUNC) &_later_cancel,              2},
+  {"_later_nextOpSecs",          (DL_FUNC) &_later_nextOpSecs,          1},
   {"_later_testCallbackOrdering", (DL_FUNC) &_later_testCallbackOrdering, 0},
+  {"_later_createCallbackRegistry", (DL_FUNC) &_later_createCallbackRegistry, 1},
+  {"_later_deleteCallbackRegistry", (DL_FUNC) &_later_deleteCallbackRegistry, 1},
+  {"_later_existsCallbackRegistry", (DL_FUNC) &_later_existsCallbackRegistry, 1},
+  {"_later_list_queue_",         (DL_FUNC) &_later_list_queue_,         1},
+  {"_later_log_level",           (DL_FUNC) &_later_log_level,           1},
   {NULL, NULL, 0}
 };
 
-void execLaterNative(void (*func)(void*), void* data, double secs);
+uint64_t execLaterNative(void (*func)(void*), void* data, double secs);
+uint64_t execLaterNative2(void (*func)(void*), void* data, double secs, int loop);
+int apiVersion();
 
 void R_init_later(DllInfo *dll)
 {
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
-  R_RegisterCCallable("later", "execLaterNative", (DL_FUNC)&execLaterNative);
+  // 2019-08-06
+  // execLaterNative is registered here ONLY for backward compatibility; If
+  // someone installed a package which had `#include <later_api.h>` (like
+  // httpuv) then that package would have compiled the inline functions from
+  // inst/include/later.h, which in turn used `R_GetCCallable("later",
+  // "execLaterNative")`, then called that function with 3 arguments. For
+  // anyone who upgrades this package but does not upgrade the downstream
+  // dependency, that interface cannot change.
+  //
+  // So we register `execLaterNative` here, even though we don't actually call
+  // it from inst/include/later.h anymore. This ensures that downstream deps
+  // that were built with the previous version can still use
+  // `R_GetCCallable("later", "execLaterNative")` and have it work properly.
+  //
+  // In a future version, after no one is running downstream packages that are
+  // built against the previous version of later, we can remove this line.
+  //
+  // https://github.com/r-lib/later/issues/97
+  R_RegisterCCallable("later", "execLaterNative",  (DL_FUNC)&execLaterNative);
+  R_RegisterCCallable("later", "execLaterNative2", (DL_FUNC)&execLaterNative2);
+  R_RegisterCCallable("later", "apiVersion",       (DL_FUNC)&apiVersion);
 }
--- src/later.h
+++ src/later.h
@@ -1,10 +1,27 @@
 #ifndef _LATER_H_
 #define _LATER_H_
 
-bool execCallbacks(double timeoutSecs = 0, bool runAll = true);
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include "callback_registry.h"
+
+// This should be kept in sync with LATER_H_API_VERSION in
+// inst/include/later.h. Whenever the interface between inst/include/later.h
+// and the code in src/ changes, these values should be incremented.
+#define LATER_DLL_API_VERSION 2
+
+#define GLOBAL_LOOP 0
+
+boost::shared_ptr<CallbackRegistry> getCallbackRegistry(int loop);
+
 bool execCallbacksForTopLevel();
 bool at_top_level();
-bool idle();
-extern "C" void execLaterNative(void (*func)(void*), void* data, double secs);
+
+bool execCallbacks(double timeoutSecs = 0, bool runAll = true, int loop = GLOBAL_LOOP);
+bool idle(int loop);
+
+extern "C" uint64_t execLaterNative(void (*func)(void*), void* data, double secs);
+extern "C" uint64_t execLaterNative2(void (*func)(void*), void* data, double secs, int loop);
+extern "C" int apiVersion();
 
 #endif // _LATER_H_
