a77a387766ab24057c9400a061d60500a117fbf9
Joe Cheng <joe@rstudio.com>
2020-06-05 09:10:13 +0000

    version 1.1.0.1

--- tests/testthat/test-cancel.R
+++ tests/testthat/test-cancel.R
@@ -29,7 +29,6 @@ test_that("Cancelling callbacks", {
 
 
 test_that("Cancelled functions will be GC'd", {
-  l <- create_loop(autorun = FALSE)
   x <- 0
   cancel <- later(
     local({
@@ -94,7 +93,9 @@ test_that("Cancelling callbacks on temporary event loops", {
     expect_identical(x, 1)
   })
 
-  # Canceling after an event loop has been destroyed should return FALSE
+  # Canceling after an event loop handle has been destroyed: the underlying
+  # data structure (in C++) will be deleted, along with the callbacks. This is
+  # true because the loop does not have a parent.
   cancel <- NULL
   x <- 0
   with_temp_loop({
@@ -105,9 +106,9 @@ test_that("Cancelling callbacks on temporary event loops", {
 })
 
 
-test_that("Cancelling callbacks on persistent private loops", {
-  l1 <- create_loop(autorun = FALSE)
-  l2 <- create_loop(autorun = FALSE)
+test_that("Cancelling callbacks on persistent private loops without parent", {
+  l1 <- create_loop(parent = NULL)
+  l2 <- create_loop(parent = NULL)
 
   # Cancel from outside with_loop
   cancel <- NULL
@@ -138,7 +139,7 @@ test_that("Cancelling callbacks on persistent private loops", {
 
 
   # Cancelling on an explicitly destroyed loop returns FALSE
-  l3 <- create_loop(autorun = FALSE)
+  l3 <- create_loop(parent = NULL)
   cancel <- NULL
   x <- 0
   with_loop(l3, {
@@ -147,4 +148,53 @@ test_that("Cancelling callbacks on persistent private loops", {
   destroy_loop(l3)
   expect_false(cancel())
   expect_identical(x, 0)
+})
+
+test_that("Cancelling callbacks on persistent private loops with parent", {
+  # If the loop handle is GC'd but the loop _does have_ a parent, then the
+  # underlying objects will not be destroyed right away, so the cancel() will
+  # work.
+  cancel <- NULL
+  x <- 0
+  local({
+    l1 <- create_loop(parent = current_loop())
+    cancel <<- later(function() { x <<- x + 1 }, loop = l1)
+  })
+  expect_true(cancel())
+  expect_false(cancel())
+  expect_identical(x, 0)
+})
+
+test_that("A canceler will not keep loop alive", {
+  l <- create_loop(parent = NULL)
+  finalized <- FALSE
+
+  reg.finalizer(l, function(x) finalized <<- TRUE)
+  cancel <- later(function() 1, loop = l)
+  rm(l)
+  gc()
+  expect_true(finalized)
+})
+
+test_that("Canceling a callback from another a callback", {
+  # Canceling a callback from another callback should work. Additionally, the
+  # altered ordering of callbacks shouldn't prevent other callbacks from
+  # running. In this test, #1 cancels 2 and 3, but we still expect 4 to run. If
+  # we used the wrong algorithm for traversing the queue and canceling
+  # callbacks, it would be possible for the cancellation of 2 and 3 to cause 4
+  # to not run. This test ensures that we do it the right way.
+  ran_2 <- FALSE
+  ran_3 <- FALSE
+  ran_4 <- FALSE
+  with_temp_loop({
+    cancel_1 <- later(function() { cancel_2(); cancel_3() })
+    cancel_2 <- later(function() { ran_2 <<- TRUE })
+    cancel_3 <- later(function() { ran_3 <<- TRUE })
+    later(function() { ran_4 <<- TRUE })
+    run_now()
+  })
+
+  expect_false(ran_2)
+  expect_false(ran_3)
+  expect_true(ran_4)
 })
